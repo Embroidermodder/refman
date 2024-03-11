b4ec59c2f5d1ee7e8ef584859f6ea5b68a14e842
Robin Swift <robinswiftart@gmail.com>
2024-03-11 13:53:11 +0000

    moving over from website: images, icons, data

---
+++ scripts/deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing required dependencies..."
+
+sudo apt-get -y update || exit 1
+sudo apt-get -y upgrade || exit 1
+sudo apt-get -y install texlive texlive-latex-base texlive-latex-extra &>> deps.log || exit 1
+
+echo "Dependencies installed."
